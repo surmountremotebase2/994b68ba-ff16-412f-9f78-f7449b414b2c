e606112e5257d8046085aba49258cc65b6cfcb87
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-05-31 13:25:33 -0400

    User Ayush Rautwar saved changes to main.py at 05/31/2024, 17:25:32 UTC time.

--- 8314b174-d8f0-4dc5-ad9f-9f6d1c1bc0d2/main.py
+++ 8314b174-d8f0-4dc5-ad9f-9f6d1c1bc0d2/main.py
@@ -1,1 +1,17 @@
-#Type code here
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ["AAPL", ...]
+        self.data_list = [...]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
